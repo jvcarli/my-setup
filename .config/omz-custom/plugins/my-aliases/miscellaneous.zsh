189efb28e42eb2bce9267cf5b250cc0e917a4527
Joao Vitor Carli <jvitor.carli@tutamail.com>
2021-03-19 11:48:50 -0300

    Fix ptmux alias path on my-aliases omz plugin.

--- .config/omz-custom/plugins/my-aliases/miscellaneous.zsh
+++ .config/omz-custom/plugins/my-aliases/miscellaneous.zsh
@@ -7,5 +7,5 @@
 alias difrati="ssh -p 22 difrat96@difrati.com.br"
 alias itmux="tmux -CC"  # alias for opening tmux in conm2 integration
 alias icat="kitty +kitten icat"
-alias ptmux="$HOME/.config/omz-custom/plugins/workflows/tmux-py-workflow.sh"
+alias ptmux="$HOME/.config/omz-custom/plugins/tmux/tmux-py-workflow.sh"
 alias clear="/usr/bin/clear"
