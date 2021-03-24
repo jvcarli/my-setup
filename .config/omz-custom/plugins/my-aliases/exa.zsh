c9592fed4ecdf619dbecacdcd0ff3d5cdf0d7f47
João Vítor Carli <jvitor.carli@tutamail.com>
2021-03-24 10:22:53 -0300

    Add exa cli aliases to omz custom plugin.

---
+++ .config/omz-custom/plugins/my-aliases/exa.zsh
@@ -0,0 +1,8 @@
+# Alias for EXA command line tool (ls replacement)
+
+alias ls="exa --icons"
+alias l="exa -l --icons"
+alias lt="exa --tree --icons"
+alias la="exa -a --icons"
+alias lg="exa -la --icons | rg"
+
--- .config/omz-custom/plugins/my-aliases/my-aliases.plugin.zsh
+++ .config/omz-custom/plugins/my-aliases/my-aliases.plugin.zsh
@@ -2,3 +2,4 @@
 
 source $HOME/.config/omz-custom/plugins/my-aliases/lsd.zsh
 source $HOME/.config/omz-custom/plugins/my-aliases/miscellaneous.zsh
+source $HOME/.config/omz-custom/plugins/my-aliases/exa.zsh
