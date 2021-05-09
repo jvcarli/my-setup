f1e2e58dd6f48a7df191381e294aba012aa8d46c
João Vítor Carli <jvitor.carli@tutamail.com>
2021-05-09 18:02:15 -0300

    Add neovim mappings config directory

---
+++ .config/nvim/lua/mappings/init.lua
@@ -0,0 +1,5 @@
+-- https://github.com/mattn/emmet-vim
+-- Redefine emmet-vim trigger key
+-- Note that the trailing `,` (comma) still needs to be entered
+-- so <C-S> will be `ctrl + s > ,`
+vim.g.user_emmet_leader_key='<C-S>'
